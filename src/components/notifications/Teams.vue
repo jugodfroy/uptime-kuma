73560d4e4bfbb59ca5d24c2ffc969325a2b3348f
jgodfroy <jgodfroy@iliad-free.fr>
2025-08-25 18:05:30 +0200

    update MS doc for new webhook

--- src/components/notifications/Teams.vue
+++ src/components/notifications/Teams.vue
@@ -10,7 +10,7 @@
         />
         <i18n-t tag="div" keypath="wayToGetTeamsURL" class="form-text">
             <a
-                href="https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook"
+                href="https://support.microsoft.com/en-us/office/create-incoming-webhooks-with-workflows-for-microsoft-teams-8ae491c7-0394-4861-ba59-055e33f75498"
                 target="_blank"
             >{{ $t("here") }}</a>
         </i18n-t>
